3cfcebb6bbdcdbadf67aa946446fcec99fb6b8c8
John McKie <john.mckie@remarkable.net>
2018-06-11 17:05:33 +0100

    Challenge One

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,24 @@
 console.log('app.js is running');
 
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ul>
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li>List item 3</li>
+        </ul>
+    </div>);
+
 var appRoot = document.getElementById('app');
 
-
 ReactDOM.render(template, appRoot);
+
+// create a new templateTwo var JSX expression
+// div
+// h1 tag
+// -- My name
+// p tag -- age: age
+//p location: Nottingham
+//  Render template2 instead of template
